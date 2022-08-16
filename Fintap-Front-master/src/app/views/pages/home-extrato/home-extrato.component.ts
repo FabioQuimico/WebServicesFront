0d023d28af7c1e00d84635b46ef20fbe006ae7c6
Mestre-Bio <omestrebio@gmail.com>
2022-08-15 22:54:50 -0300

    Single Page funcional

--- Fintap-Front-master/src/app/views/pages/home-extrato/home-extrato.component.ts
+++ Fintap-Front-master/src/app/views/pages/home-extrato/home-extrato.component.ts
@@ -10,13 +10,19 @@ export class HomeExtratoComponent implements OnInit {
   // nomeCliente deve vir da tela de Login
   nomeCliente = "Página de extrato do cliente";
   // mes e ano devem vir da data atual
-  mes: number = new Date().getMonth() + 1;
-  ano: number = new Date().getFullYear();
+  mes: number = 0;
+  ano: number = 0;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    if (this.mes == 0) {
+      this.mes = new Date().getMonth() + 1;
+    }
+    if (this.ano == 0) {
+      this.ano = new Date().getFullYear();
+    }
   }
 
   setMes(mes: number): void {

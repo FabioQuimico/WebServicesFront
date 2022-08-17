2310d95eae3bce8798764ceea277bc67fec4fbe4
Rafael Braga S Ferreira <ferreirabraga@gmail.com>
2022-08-16 23:30:41 -0300

    Destaca ano e mes selecionados

--- Fintap-Front-master/src/app/views/pages/home-extrato/home-extrato.component.ts
+++ Fintap-Front-master/src/app/views/pages/home-extrato/home-extrato.component.ts
@@ -12,24 +12,35 @@ export class HomeExtratoComponent implements OnInit {
   // mes e ano devem vir da data atual
   mes: number = 0;
   ano: number = 0;
-
+  isSelectedMes: number = 0;
+  isSelectedAno: number = 0;
+  meses:number[]= [1,2,3,4,5,6,7,8,9,10,11,12] 
+  mesesNome:string[]= ["Jan", "Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"] 
+  anos:number[]= [2020,2021,2022] 
   constructor() {
   }
 
   ngOnInit(): void {
     if (this.mes == 0) {
       this.mes = new Date().getMonth() + 1;
+      this.isSelectedMes = this.mes;
     }
     if (this.ano == 0) {
       this.ano = new Date().getFullYear();
+      this.isSelectedAno = this.ano;
     }
   }
 
   setMes(mes: number): void {
     this.mes = mes;
+    this.isSelectedMes = mes;
+    console.log(this.isSelectedMes+" = "+ mes)
   }
   setAno(ano: number): void {
     this.ano = ano;
+    this.isSelectedAno = ano;
+    console.log(this.isSelectedAno+" = "+ ano)
   }
+  
 
 }
